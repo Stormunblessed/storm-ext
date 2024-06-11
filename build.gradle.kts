55a93ef4b696529b10b71da9f4dee4c803b3cf73
Stormunblessed <stormunblessed@gmail.com>
2024-06-10 21:31:32 -0600

    bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -78,10 +78,10 @@ subprojects {
         // but you dont need to include any of them if you dont need them
         // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle
         implementation(kotlin("stdlib")) // adds standard kotlin features, like listOf, mapOf etc
-        implementation("com.github.Blatzar:NiceHttp:0.3.2") // http library
+        implementation("com.github.Blatzar:NiceHttp:0.4.11") // http library
         implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
-        implementation("org.jsoup:jsoup:1.13.1") // html parser
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4") // delay()
+        implementation("org.jsoup:jsoup:1.17.2") // html parser
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0") // delay()
 
         //run JS
         implementation("org.mozilla:rhino:1.7.14")
